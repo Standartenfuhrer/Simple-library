5493ed849a655b4e9fedf98e28304855daf16f35
Standartenfuhrer <tamerlandzigkaev@gmail.com>
2025-10-27 13:21:31 +0300

    Refactoring all

--- main.go
+++ main.go
@@ -1,82 +1,32 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	//"github.com/Standartenfuhrer/simple-library/domain"
+	"github.com/Standartenfuhrer/simple-library/library"
+)
 
 func main() {
-	fmt.Println("Запуск системы управления библиотекой...")
+	myLibrary := library.New()
 
-	//Создаем библиотеку
-	myLibrary := &Library{}
-
-	fmt.Println("\n--- Наполняем библиотеку ---")
-	//Добавляем пользователей
 	reader, err := myLibrary.AddReader("Тамерлан", "Джигкаев")
-	if err != nil{
-		fmt.Println(err)
-	} else {
-		fmt.Println("Зарегестрирован новый читатель:", reader)
-	}
-	reader, err = myLibrary.AddReader("Линда", "Элбакянц")
-	if err != nil{
+	if err != nil {
 		fmt.Println(err)
 	} else {
 		fmt.Println("Зарегестрирован новый читатель:", reader)
 	}
 
-	//Добавляем пользователей
-	b, err := myLibrary.AddBook(1833, "Евгений Онегин", "Александр Пушкин")
-	if err != nil{
+	b, err := myLibrary.AddBook(1833, "Егвений Онегин", "Александр Пушкин")
+	if err != nil {
 		fmt.Println(err)
 	} else {
-		fmt.Printf("Книга (%v) успешно добавлена.", b)
+		fmt.Printf("Книга (%v) успешно добавлена\n", b.Title)
 	}
-	b, err = myLibrary.AddBook(1984, "1984", "Джордж Оруэлл")
-	if err != nil{
-		fmt.Println(err)
-	} else {
-		fmt.Printf("\nКнига (%v) успешно добавлена.", b)
-	}
-	b, err = myLibrary.AddBook(1967, "Мастер и Маргарита", "Михаил Булгаков")
-	if err != nil{
-		fmt.Println(err)
-	} else {
-		fmt.Printf("\nКнига (%v) успешно добавлена.\n", b)
-	}
-	
-	//Выдаем книгу пользователю
-	fmt.Println("----Успешная выдача книги----")
+
 	err = myLibrary.IssueBookToReader(1, 1)
-	if err != nil{
+	if err != nil {
 		fmt.Println(err)
 	} else {
-		fmt.Println(myLibrary.Books[1])
-	}
-	fmt.Println("----Попытка выдать уже выданную книгу----")
-	err = myLibrary.IssueBookToReader(1, 1)
-	if err != nil{
-		fmt.Println(err)
-	} else {
-		fmt.Println(myLibrary.Books[1])
-	}
-	fmt.Println("----Попытка выдать книгу несуществующему читателю----")
-	err = myLibrary.IssueBookToReader(2, 15)
-	if err != nil{
-		fmt.Println(err)
-	} else {
-		fmt.Println(myLibrary.Books[1])
-	}
-	fmt.Println("----Успешный возврат книги----")
-	err = myLibrary.ReturnBook(1)
-	if err != nil{
-		fmt.Println(err)
-	} else {
-		fmt.Println("Статус книги после возварат:", myLibrary.Books[1])
-	}
-	fmt.Println("----Попытка вернуть книгу, которая уже в библиотеке----")
-	err = myLibrary.ReturnBook(1)
-	if err != nil{
-		fmt.Println(err)
-	} else {
-		fmt.Println("Статус книги после возварат:", myLibrary.Books[1])
+		fmt.Println(myLibrary.Books[0])
 	}
 }
