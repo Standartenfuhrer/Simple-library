0bebb7d4f66a7f00e32106e1c46dacbe604d0b4a
Standartenfuhrer <tamerlandzigkaev@gmail.com>
2025-11-24 14:45:25 +0300

    Add: func homeHandler, healthCheckHandler, infoHandler, notFoundHandler

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -5,13 +5,17 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"time"
 )
 
 func main() {
 	//Регистрируем обработчик для пути "/"
 	//Все запросы на адрес http://localhost/8080 будут обрабатываться функцией homeHandler
-	http.HandleFunc("/", healthCheckHandler)
-
+	http.HandleFunc("/", homeHandler)
+	http.HandleFunc("/health_check", healthCheckHandler)
+	http.HandleFunc("/info", infoHandler)
+	http.HandleFunc("/notfound", notFoundHandler)
+	
 	//Определяем порт, на котором будет работать сервер
 	port := ":8080"
 	fmt.Printf("Запускаем сервер на порту %s", port)
@@ -25,9 +29,9 @@ func main() {
 }
 
 // Наш первый обработчик
-// func homeHandler(w http.ResponseWriter, r *http.Request) {
-// 	w.Write([]byte("Hello from Library"))ss
-// }
+func homeHandler(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Hello from Library"))
+}
 
 func healthCheckHandler(w http.ResponseWriter, r *http.Request) {
 	//1. Создаем структуру для ответа, чтобы ее было легко превратить в JSON
@@ -54,4 +58,33 @@ func healthCheckHandler(w http.ResponseWriter, r *http.Request) {
 
 	//5. Пишем JSON для ответа
 	w.Write(jsonData)
+}
+
+//Вывод информации
+func infoHandler(w http.ResponseWriter, r *http.Request) {
+	response := struct {
+		Status     string `json:"status"`
+		Version    string `json:"version"`
+		ServerTime string `json:"server_time"`
+	}{
+		Status:     "available",
+		Version:    "1.0",
+		ServerTime: time.Now().Format(time.RFC3339),
+	}
+
+	jsonData, err := json.MarshalIndent(response, "", " ")
+	if err != nil {
+		http.Error(w, "Internal Server Error", 500)
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+
+	w.WriteHeader(http.StatusOK)
+
+	w.Write(jsonData)
+}
+//Не найдена страница
+func notFoundHandler(w http.ResponseWriter, r *http.Request){
+	w.WriteHeader(http.StatusNotFound)
+	w.Write([]byte("Страница не найдена"))
 }
